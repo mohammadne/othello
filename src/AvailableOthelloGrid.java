d0ecfc1c69972f1dfe26dd7c43e5e4b5845645ff
Nasr <mohammadne@mail.ru>
2020-04-13 20:52:46 +0430

    finish and run correctly

--- src/AvailableOthelloGrid.java
+++ src/AvailableOthelloGrid.java
@@ -1,13 +1,15 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.util.Map.entry;
+
 class AvailableOthelloGrid {
 
     private static boolean areTheSame(Turn turn, OthelloGrid grid) {
         if (turn == Turn.WHITE) {
             return grid == OthelloGrid.WHITE;
         }
-        return grid == OthelloGrid.WHITE;
+        return grid == OthelloGrid.BLACK;
     }
 
     private static int calculateAvailableByList(Turn turn, OthelloGrid[] othelloGrid) {
@@ -23,6 +25,14 @@ class AvailableOthelloGrid {
         return 0;
     }
 
+    private static OthelloGrid[] reverseArray(OthelloGrid[] grids) {
+        OthelloGrid[] temp = new OthelloGrid[grids.length];
+        for (int i = 0; i < grids.length; i++) {
+            temp[i] = grids[grids.length - 1 - i];
+        }
+        return temp;
+    }
+
     private static OthelloGrid[] remainingGridsRight(int index, OthelloGrid[] allGrids) {
         OthelloGrid[] remaining = new OthelloGrid[allGrids.length - index - 1];
         System.arraycopy(allGrids, index + 1, remaining, 0, remaining.length);
@@ -32,27 +42,31 @@ class AvailableOthelloGrid {
     private static OthelloGrid[] remainingGridsLeft(int index, OthelloGrid[] allGrids) {
         OthelloGrid[] remaining = new OthelloGrid[index];
         System.arraycopy(allGrids, 0, remaining, 0, remaining.length);
-        return remaining;
+        return reverseArray(remaining);
     }
 
-    private static OthelloGrid[] remainingGridsUp(int index, OthelloGrid[] allGrids) {
-        OthelloGrid[] remaining = new OthelloGrid[index];
-        System.arraycopy(allGrids, 0, remaining, 0, remaining.length);
-        return remaining;
+    private static OthelloGrid[] remainingGridsUp(int row, int column, OthelloGrid[][] othelloGrid) {
+        OthelloGrid[] remaining = new OthelloGrid[row];
+        for (int i = 0; i < row; i++) {
+            remaining[i] = othelloGrid[i][column];
+        }
+        return reverseArray(remaining);
     }
 
-    private static OthelloGrid[] remainingGridsDown(int index, OthelloGrid[] allGrids) {
-        OthelloGrid[] remaining = new OthelloGrid[allGrids.length - index - 1];
-        System.arraycopy(allGrids, index + 1, remaining, 0, remaining.length);
+    private static OthelloGrid[] remainingGridsDown(int row, int column, OthelloGrid[][] othelloGrid) {
+        OthelloGrid[] remaining = new OthelloGrid[7 - row];
+        for (int i = 0; i < 7 - row; i++) {
+            remaining[i] = othelloGrid[i + row + 1][column];
+        }
         return remaining;
     }
 
     private static OthelloGrid[] remainingGridsUpRight(int row, int column, OthelloGrid[][] othelloGrid) {
-        int possibleRows = 7 - row;
-        int length = Math.min(possibleRows, column);
+        int possibleRight = 7 - column;
+        int length = Math.min(possibleRight, row);
         OthelloGrid[] remaining = new OthelloGrid[length];
         for (int i = 0; i < length; i++) {
-            remaining[i] = othelloGrid[row + 1 + i][column - 1 - i];
+            remaining[i] = othelloGrid[row - 1 - i][column + 1 + i];
         }
         return remaining;
     }
@@ -67,9 +81,9 @@ class AvailableOthelloGrid {
     }
 
     private static OthelloGrid[] remainingGridsDownRight(int row, int column, OthelloGrid[][] othelloGrid) {
-        int possibleRows = 7 - row;
-        int possibleColumns = 7 - column;
-        int length = Math.min(possibleRows, possibleColumns);
+        int possibleRight = 7 - row;
+        int possibleDown = 7 - column;
+        int length = Math.min(possibleRight, possibleDown);
         OthelloGrid[] remaining = new OthelloGrid[length];
         for (int i = 0; i < length; i++) {
             remaining[i] = othelloGrid[row + 1 + i][column + 1 + i];
@@ -78,76 +92,97 @@ class AvailableOthelloGrid {
     }
 
     private static OthelloGrid[] remainingGridsDownLeft(int row, int column, OthelloGrid[][] othelloGrid) {
-        int possibleColumns = 7 - column;
-        int length = Math.min(row, possibleColumns);
+        int possibleDown = 7 - row;
+        int length = Math.min(column, possibleDown);
         OthelloGrid[] remaining = new OthelloGrid[length];
         for (int i = 0; i < length; i++) {
-            remaining[i] = othelloGrid[row - 1 - i][column + 1 + i];
+            remaining[i] = othelloGrid[row + 1 + i][column - 1 - i];
         }
         return remaining;
     }
 
-    static Map<Integer, Integer> available(Turn turn, OthelloGrid[][] othelloGrid) {
+    static Map<Integer, Map<String, Integer>> available(Turn turn, OthelloGrid[][] othelloGrid) {
         /*
          *first Integer => grid is 2 length
          * second Integer => possible grids to turn is 1 length
          */
-        Map<Integer, Integer> availableGrids = new HashMap<>();
+        Map<Integer, Map<String, Integer>> availableGrids = new HashMap<>();
 
 
         for (int row = 0; row < 8; row++) {
             for (int column = 0; column < 8; column++) {
+                /*
+                 *it only looks for the same grids and
+                 * calculate how many grids are there in 8 position of
+                 * that same grid
+                 */
                 if (areTheSame(turn, othelloGrid[row][column])) {
                     OthelloGrid[] remaining;
                     // right
-                    remaining = remainingGridsRight(row, othelloGrid[row]);
+                    remaining = remainingGridsRight(column, othelloGrid[row]);
                     int right = calculateAvailableByList(turn, remaining);
                     if (right != 0) {
-                        int key = (row + right + 1) * 10 + column;
+                        int key = row * 10 + (column + right + 1);
                         if (availableGrids.containsKey(key)) {
-                            int temp = availableGrids.get(key);
-                            availableGrids.put(key, right + temp);
+                            Map<String, Integer> temp = new HashMap<>(availableGrids.get(key));
+                            temp.put("left", right);
+                            availableGrids.put(key, temp);
                         } else {
-                            availableGrids.put(key, right);
+                            Map<String, Integer> temp = Map.ofEntries(
+                                    entry("left", right)
+                            );
+                            availableGrids.put(key, temp);
                         }
                     }
 
                     // left
-                    remaining = remainingGridsLeft(row, othelloGrid[row]);
+                    remaining = remainingGridsLeft(column, othelloGrid[row]);
                     int left = calculateAvailableByList(turn, remaining);
                     if (left != 0) {
-                        int key = (row + left - 1) * 10 + column;
+                        int key = row * 10 + (column - left - 1);
                         if (availableGrids.containsKey(key)) {
-                            int temp = availableGrids.get(key);
-                            availableGrids.put(key, left + temp);
+                            Map<String, Integer> temp = new HashMap<>(availableGrids.get(key));
+                            temp.put("right", left);
+                            availableGrids.put(key, temp);
                         } else {
-                            availableGrids.put(key, left);
+                            Map<String, Integer> temp = Map.ofEntries(
+                                    entry("right", left)
+                            );
+                            availableGrids.put(key, temp);
                         }
                     }
 
                     // up
-                    remaining = remainingGridsUp(row, othelloGrid[row]);
+                    remaining = remainingGridsUp(row, column, othelloGrid);
                     int up = calculateAvailableByList(turn, remaining);
                     if (up != 0) {
-                        int key = row * 10 + (column - up - 1);
+                        int key = (row - up - 1) * 10 + column;
                         if (availableGrids.containsKey(key)) {
-                            int temp = availableGrids.get(key);
-                            availableGrids.put(key, up + temp);
+                            Map<String, Integer> temp = new HashMap<>(availableGrids.get(key));
+                            temp.put("down", up);
+                            availableGrids.put(key, temp);
                         } else {
-                            availableGrids.put(key, up);
+                            Map<String, Integer> temp = Map.ofEntries(
+                                    entry("down", up)
+                            );
+                            availableGrids.put(key, temp);
                         }
                     }
 
                     // down
-                    remaining = remainingGridsDown(row, othelloGrid[row]);
+                    remaining = remainingGridsDown(row, column, othelloGrid);
                     int down = calculateAvailableByList(turn, remaining);
                     if (down != 0) {
-                        int key = row * 10 + (column + down + 1);
+                        int key = (row + down + 1) * 10 + column;
                         if (availableGrids.containsKey(key)) {
-                            int temp = availableGrids.get(key);
-                            availableGrids.put(key, down + temp);
+                            Map<String, Integer> temp = new HashMap<>(availableGrids.get(key));
+                            temp.put("up", down);
+                            availableGrids.put(key, temp);
                         } else {
-                            availableGrids.put(key, down);
+                            Map<String, Integer> temp = Map.ofEntries(
+                                    entry("up", down)
+                            );
+                            availableGrids.put(key, temp);
                         }
                     }
 
@@ -155,12 +190,16 @@ class AvailableOthelloGrid {
                     remaining = remainingGridsUpRight(row, column, othelloGrid);
                     int upRight = calculateAvailableByList(turn, remaining);
                     if (upRight != 0) {
-                        int key = (row + upRight + 1) * 10 + (column - upRight - 1);
+                        int key = (row - upRight - 1) * 10 + (column + upRight + 1);
                         if (availableGrids.containsKey(key)) {
-                            int temp = availableGrids.get(key);
-                            availableGrids.put(key, upRight + temp);
+                            Map<String, Integer> temp = new HashMap<>(availableGrids.get(key));
+                            temp.put("downLeft", upRight);
+                            availableGrids.put(key, temp);
                         } else {
-                            availableGrids.put(key, upRight);
+                            Map<String, Integer> temp = Map.ofEntries(
+                                    entry("downLeft", upRight)
+                            );
+                            availableGrids.put(key, temp);
                         }
                     }
 
@@ -170,10 +209,14 @@ class AvailableOthelloGrid {
                     if (upLeft != 0) {
                         int key = (row - upLeft - 1) * 10 + (column - upLeft - 1);
                         if (availableGrids.containsKey(key)) {
-                            int temp = availableGrids.get(key);
-                            availableGrids.put(key, upLeft + temp);
+                            Map<String, Integer> temp = new HashMap<>(availableGrids.get(key));
+                            temp.put("downRight", upLeft);
+                            availableGrids.put(key, temp);
                         } else {
-                            availableGrids.put(key, upLeft);
+                            Map<String, Integer> temp = Map.ofEntries(
+                                    entry("downRight", upLeft)
+                            );
+                            availableGrids.put(key, temp);
                         }
                     }
 
@@ -183,10 +226,14 @@ class AvailableOthelloGrid {
                     if (downRight != 0) {
                         int key = (row + downRight + 1) * 10 + (column + downRight + 1);
                         if (availableGrids.containsKey(key)) {
-                            int temp = availableGrids.get(key);
-                            availableGrids.put(key, downRight + temp);
+                            Map<String, Integer> temp = new HashMap<>(availableGrids.get(key));
+                            temp.put("upLeft", downRight);
+                            availableGrids.put(key, temp);
                         } else {
-                            availableGrids.put(key, downRight);
+                            Map<String, Integer> temp = Map.ofEntries(
+                                    entry("upLeft", downRight)
+                            );
+                            availableGrids.put(key, temp);
                         }
                     }
 
@@ -194,15 +241,18 @@ class AvailableOthelloGrid {
                     remaining = remainingGridsDownLeft(row, column, othelloGrid);
                     int downLeft = calculateAvailableByList(turn, remaining);
                     if (downLeft != 0) {
-                        int key = (row - downLeft - 1) * 10 + (column + downLeft + 1);
+                        int key = (row + downLeft + 1) * 10 + (column - downLeft - 1);
                         if (availableGrids.containsKey(key)) {
-                            int temp = availableGrids.get(key);
-                            availableGrids.put(key, downLeft + temp);
+                            Map<String, Integer> temp = new HashMap<>(availableGrids.get(key));
+                            temp.put("upRight", downLeft);
+                            availableGrids.put(key, temp);
                         } else {
-                            availableGrids.put(key, downLeft);
+                            Map<String, Integer> temp = Map.ofEntries(
+                                    entry("upRight", downLeft)
+                            );
+                            availableGrids.put(key, temp);
                         }
                     }
-
 
                 }
             }
--- src/Main.java
+++ src/Main.java
@@ -20,30 +20,57 @@ public class Main {
                 othelloGrid[row][column] = OthelloGrid.NONE;
             }
         }
-        othelloGrid[3][3] = OthelloGrid.BLACK;
-        othelloGrid[4][4] = OthelloGrid.BLACK;
-        othelloGrid[3][4] = OthelloGrid.WHITE;
-        othelloGrid[4][3] = OthelloGrid.WHITE;
+        othelloGrid[3][3] = OthelloGrid.WHITE;
+        othelloGrid[4][4] = OthelloGrid.WHITE;
+        othelloGrid[3][4] = OthelloGrid.BLACK;
+        othelloGrid[4][3] = OthelloGrid.BLACK;
     }
 
     public static void main(String[] args) {
-        Turn turn = Turn.WHITE;
-        Map<Integer, Integer> avilables = AvailableOthelloGrid.available(turn, othelloGrid);
-//        while (true) {
-//            if (true) {
-//                Scanner input = new Scanner(System.in);
-//            } else {
-//                if (true){
-//
-//                } else {
-//                    System.out.println("برنده کیست ؟");
-//                    break;
-//                }
-//            }
-//        }
+        Turn turn = Turn.BLACK;
 
 
-        OutPut a = new OutPut(othelloGrid);
-        a.printResult();
+        while (true) {
+            Map<Integer, Map<String, Integer>> available = AvailableOthelloGrid.available(turn, othelloGrid);
+            OutPut.printResult(othelloGrid);
+            System.out.println(turn.toString());
+            InputOutPut.printOutPut(available);
+
+            if (available.isEmpty()) {
+                turn = toggleTurn(turn);
+                System.out.println("Pass");
+                available = AvailableOthelloGrid.available(turn, othelloGrid);
+                if (available.isEmpty()) {
+                    Map<Turn, Integer> temp = calculateWinner.calculate(othelloGrid);
+                    int whiteTurn = temp.get(Turn.WHITE);
+                    int blackTurn = temp.get(Turn.BLACK);
+                    if (whiteTurn > blackTurn) {
+                        System.out.println("whiteTurn is winner " + whiteTurn);
+                    } else if (blackTurn > whiteTurn) {
+                        System.out.println("blackTurn is winner " + blackTurn);
+                    } else {
+                        System.out.println("Equals " + whiteTurn);
+                    }
+                    break;
+                }
+            } else {
+
+                int num = InputOutPut.getInput();
+                if (available.containsKey(num)) {
+                    othelloGrid = Insert.insertion(turn, othelloGrid, available, num);
+                    turn = toggleTurn(turn);
+                } else {
+                    System.out.println("This Grid Is Not Available For You , Try Again");
+                }
+            }
+        }
+
     }
+
+    private static Turn toggleTurn(Turn turn) {
+        if (turn == Turn.WHITE) return Turn.BLACK;
+        return Turn.WHITE;
+    }
+
+
 }
